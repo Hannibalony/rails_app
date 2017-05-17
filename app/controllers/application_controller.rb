5730756b5107d874f68ebaf894f06b0ba7dfdf3f
Hannibalony <justinecantado@gmail.com>
2017-05-17 00:42:02 +0000

    new render text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def goodbye
-    render html: "new text"
+    render html: "test text"
   end
  
 end
